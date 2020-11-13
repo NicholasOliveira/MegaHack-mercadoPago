a8d52274c8919eda458be4c31a5dbb4dc249b817
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-11-13 15:28:14 -0300

    subindo build

---
+++ build/precache-manifest.9a6d49de160cbe53787e0a537e9075e4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e5282d70752e6cb67bfce7fdbfb923",
+    "url": "/MegaHack-mercadoPago/build/index.html"
+  },
+  {
+    "revision": "50885719ac5236131667",
+    "url": "/MegaHack-mercadoPago/build/static/js/2.441af28c.chunk.js"
+  },
+  {
+    "revision": "9d64bea21b2d331995b70fc9f60ccfe3",
+    "url": "/MegaHack-mercadoPago/build/static/js/2.441af28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23668816d212a9d4b37",
+    "url": "/MegaHack-mercadoPago/build/static/js/main.f48ded56.chunk.js"
+  },
+  {
+    "revision": "c3ab152ac5dd00af0ecb",
+    "url": "/MegaHack-mercadoPago/build/static/js/runtime-main.687e0e3c.js"
+  },
+  {
+    "revision": "6e306daa45102a240356cd68001b00e1",
+    "url": "/MegaHack-mercadoPago/build/static/media/Header.6e306daa.svg"
+  },
+  {
+    "revision": "5bc5895e9d81d2d43a2def037f7f86aa",
+    "url": "/MegaHack-mercadoPago/build/static/media/ajuda.5bc5895e.svg"
+  },
+  {
+    "revision": "6f2ba333ecd2f53a338775a486bf28d1",
+    "url": "/MegaHack-mercadoPago/build/static/media/assinaturas.6f2ba333.svg"
+  },
+  {
+    "revision": "ef9ef305e94ce42817f60df0ed527d53",
+    "url": "/MegaHack-mercadoPago/build/static/media/atividade.ef9ef305.svg"
+  },
+  {
+    "revision": "45a482bc3a19d70b7285730edc74b260",
+    "url": "/MegaHack-mercadoPago/build/static/media/back-portal-site.45a482bc.png"
+  },
+  {
+    "revision": "c69752b355b56dabb18ceea6a56de34f",
+    "url": "/MegaHack-mercadoPago/build/static/media/convide.c69752b3.svg"
+  },
+  {
+    "revision": "b2db888c167f55fde58f7ce94e5baf15",
+    "url": "/MegaHack-mercadoPago/build/static/media/desempenho.b2db888c.svg"
+  },
+  {
+    "revision": "25cccfd3f950c0f1fc515945df60f11c",
+    "url": "/MegaHack-mercadoPago/build/static/media/dinheiro.25cccfd3.svg"
+  },
+  {
+    "revision": "0d4a3fb82e3de932b0139412250fb935",
+    "url": "/MegaHack-mercadoPago/build/static/media/icoMobile.0d4a3fb8.svg"
+  },
+  {
+    "revision": "daf8efc33db75e63b322ebd20ce5979d",
+    "url": "/MegaHack-mercadoPago/build/static/media/inicio.daf8efc3.svg"
+  },
+  {
+    "revision": "99c0ceb360130b0094c35c4b9483af09",
+    "url": "/MegaHack-mercadoPago/build/static/media/link.99c0ceb3.svg"
+  },
+  {
+    "revision": "d32bd131fc96cff1886e3478baae246d",
+    "url": "/MegaHack-mercadoPago/build/static/media/negocio.d32bd131.svg"
+  },
+  {
+    "revision": "28ec5e9d6cd781adacf8826447308fc9",
+    "url": "/MegaHack-mercadoPago/build/static/media/perfil.28ec5e9d.svg"
+  },
+  {
+    "revision": "c0d0ac103289e5a3c0291851655256f1",
+    "url": "/MegaHack-mercadoPago/build/static/media/produto1.c0d0ac10.jpg"
+  },
+  {
+    "revision": "7fb017bdbed2c37dcfb030e89db7f006",
+    "url": "/MegaHack-mercadoPago/build/static/media/receber.7fb017bd.svg"
+  },
+  {
+    "revision": "5c21a69e714fc7d0a94ff58b5962c67f",
+    "url": "/MegaHack-mercadoPago/build/static/media/relatorio.5c21a69e.svg"
+  },
+  {
+    "revision": "552b9380bb3d49ece37151e5a1622370",
+    "url": "/MegaHack-mercadoPago/build/static/media/sair.552b9380.svg"
+  },
+  {
+    "revision": "a32805c7891278950fa577c416791441",
+    "url": "/MegaHack-mercadoPago/build/static/media/transferir.a32805c7.svg"
+  }
+]);
