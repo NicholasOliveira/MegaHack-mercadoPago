c5eb59660ffffdda2f984610fb2a3cb73e391593
Matheus V <matheusvrgm@gmail.com>
2020-11-14 15:10:46 -0300

    api integration

--- src/components/ListProducts/index.tsx
+++ src/components/ListProducts/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,6 +9,7 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { ProductItem } from './styles';
 import ImgProduto1 from '../../assets/images/produto1.jpg';
+import api, { ICustomerSalesData } from '../../services/MockedApi';
 
 const useStyles = makeStyles({
   root: {
@@ -42,30 +43,74 @@ const useStyles = makeStyles({
 export default function SimpleCard({ title, Info, label, chart, link }: any) {
   const classes = useStyles();
 
+  const [CustomerSalesData, setCustomerSalesData] = useState<
+  ICustomerSalesData
+  >();
+
+  useEffect(() => {
+    async function loadDataFromApi(): Promise<void> {
+      await api.get('/customer').then(async response => {
+        setCustomerSalesData(response.data[0]);
+      });
+    }
+
+    loadDataFromApi();
+  }, []);
+
+  // const products = [
+  //   {
+  //     id: '1',
+  //     img: ImgProduto1,
+  //     title: 'Sapato prateado salto baixo',
+  //     vendas: 0,
+  //     receita: 'R$ 0,00',
+  //     estoque: 5,
+  //     ultimaVenda: '12/10/2020',
+  //   },
+  //   {
+  //     id: '1',
+  //     img: ImgProduto1,
+  //     title: 'Sapato prateado salto baixo',
+  //     vendas: 0,
+  //     receita: 'R$ 0,00',
+  //     estoque: 5,
+  //     ultimaVenda: '12/10/2020',
+  //   },
+  //   {
+  //     id: '1',
+  //     img: ImgProduto1,
+  //     title: 'Sapato prateado salto baixo',
+  //     vendas: 0,
+  //     receita: 'R$ 0,00',
+  //     estoque: 5,
+  //     ultimaVenda: '12/10/2020',
+  //   },
+  // ];
+
   const products = [
     {
       id: '1',
-      img: ImgProduto1,
-      title: 'Sapato prateado salto baixo',
-      vendas: 0,
+      img: CustomerSalesData?.wharehouse[0].product.product_image,
+      title: CustomerSalesData?.wharehouse[0].product.product_name,
+      vendas: CustomerSalesData?.wharehouse[0].product.average_sell,
+      receita: `R$ 310,00`,
+      estoque: 2,
+      ultimaVenda: '12/10/2020',
+    },
+    {
+      id: '2',
+      img: CustomerSalesData?.wharehouse[0].product.product_image,
+      title: CustomerSalesData?.wharehouse[2].product.product_name,
+      vendas: CustomerSalesData?.wharehouse[2].product.average_sell,
       receita: 'R$ 0,00',
       estoque: 5,
       ultimaVenda: '12/10/2020',
     },
     {
-      id: '1',
-      img: ImgProduto1,
-      title: 'Sapato prateado salto baixo',
-      vendas: 0,
-      receita: 'R$ 0,00',
-      estoque: 5,
-      ultimaVenda: '12/10/2020',
-    },
-    {
-      id: '1',
-      img: ImgProduto1,
-      title: 'Sapato prateado salto baixo',
-      vendas: 0,
+      id: '3',
+      img: CustomerSalesData?.wharehouse[0].product.product_image,
+      title: CustomerSalesData?.wharehouse[3].product.product_name,
+      vendas: CustomerSalesData?.wharehouse[3].product.average_sell,
       receita: 'R$ 0,00',
       estoque: 5,
       ultimaVenda: '12/10/2020',
--- src/services/MockedApi.tsx
+++ src/services/MockedApi.tsx
@@ -1,8 +1,49 @@
 import axios from 'axios';
 
+export interface ICustomerSalesData {
+  name: string;
+
+  level: number;
+
+  sales_amount: number;
+
+  income: number;
+
+  income_average: number;
+
+  wharehouse: productStock[];
+}
+
+export interface productStock {
+  product: Products;
+  sell_quantity: number;
+  income: number;
+  available: number;
+  last_sale: Date;
+}
+
+export interface Products {
+  id: string;
+
+  product_name: string;
+
+  product_description: string;
+
+  product_image: string;
+
+  product_category: string;
+
+  price: number;
+
+  average_sell: number;
+
+  rating: number;
+
+  relevance: number;
+}
+
 const api = axios.create({
-  baseURL:
-    'https://iloeirzrdmjsvklc9-mock.stoplight-proxy.io/v4/financial-dashboard',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
